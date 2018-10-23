c04b95fba2d324c13713fab55de4bedc82ee9c1d
ariefluthfi79 <ariefluthfi799@gmail.com>
2018-10-23 09:44:15 +0700

    Newest changes

--- db/migrate/20181023021107_add_lattitude_longitude.rb
+++ db/migrate/20181023021107_add_lattitude_longitude.rb
@@ -1,6 +1,6 @@
 class AddLattitudeLongitude < ActiveRecord::Migration[5.2]
   def change
-    add_column :events, :latitude, :decimal, {:precision=>10, :scale=>6}
-    add_column :events, :longitude, :decimal, {:precision=>10, :scale=>6}
+    add_column :events, :latitude, :string 
+    add_column :events, :longitude, :string
   end
 end
